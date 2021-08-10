0517044367ef320c46eebf37da5115f64162e4b2
Antonio <tato_fos@hotmail.com>
2021-08-09 22:14:32 -0400

    132 Creando un listado no conectado

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
 import React from 'react'
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
+import ListItem from './components/ListItem'
 
 const styles = StyleSheet.create({
   container: {
+    margin: 15,
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
   },
+  list: {
+    alignSelf: 'stretch',
+  }
 });
+
+const data = [
+  { id: 1, desc: 'texto prueba', completed: false},
+  { id: 2, desc: 'texto prueba 2', completed: false},
+]
 
 export default () => {
   return (
     <View style={styles.container}>
-      <Text>This is My APP with Redux</Text>
-      <StatusBar style="auto" />
+      <FlatList 
+        style={styles.list}
+        data={data}
+        keyExtractor={x => String(x.id)}
+        renderItem={({ item }) => <ListItem onPress={ () => {} } desc={item.desc} />}
+      />
     </View>
   )
 }
--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,5 +1,10 @@
+const initialState = [
+  { id: 1, desc: 'todo 1', completed: false },
+  { id: 2, desc: 'todo 2', completed: true },
+  { id: 3, desc: 'todo 3', completed: false },
+]
 
-export default (state = [], action) => {
+export default (state = initialState, action) => {
   console.log(action)
   return state
 }
